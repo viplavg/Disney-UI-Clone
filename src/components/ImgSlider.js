4228cf9ecf24cb93918af7fca01a7d9908b32710
viplavg <viplavg1999@gmail.com>
2021-05-13 16:55:14 +0530

    ADD: Viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -73,7 +73,7 @@ const Wrap = styled.div`
         height: 100%;
         box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px,
         rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-        tansition-duration: 500ms;
+        transition-duration: 300ms;
 
         &:hover {
             border: 3px solid rgba(249, 249, 249, 0.8);
